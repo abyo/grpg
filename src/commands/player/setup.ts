dd73a0d3876eada78e4241dc3c1021b89c7086f7
abyo <alexolland@getcodingknowledge.com>
2021-04-20 20:40:52 +0200

    Modified player model & setup command

--- src/commands/player/setup.ts
+++ src/commands/player/setup.ts
@@ -1,61 +1,30 @@
 import { Command } from 'discord-akairo';
-import { Message, MessageEmbed, MessageAttachment } from 'discord.js';
+import { Message } from 'discord.js';
 import { Players } from '../../structures/models/Players';
-import pClasses from '../../structures/game/data/classes.json';
 
 export default class SetupCommand extends Command {
   public constructor() {
     super('setup', {
       aliases: ['setup'],
       description: {
-        content: 'The setup command allow you to choose a class for your adventure.',
-        usage: 'setup [className] <-(-s)elect>',
+        content: 'The setup command allows you to start your adventure.',
+        usage: 'setup',
         subCommand: '',
-        examples: ['setup Clerc', 'setup Clerc --select', 'setup Clerc -s']
+        examples: ['setup']
       },
       category: 'Player',
       ratelimit: 2,
-      args: [
-        {
-          id: 'className'
-        },
-        {
-          id: 'select',
-          match: 'flag',
-          flag: ['--select', '-s']
-        }
-      ]
     });
   }
 
-  public async exec(message: Message, { className, select }: { className: string, select: string }): Promise<Message | Message[]> {
-    if (!className) return message.util!.send(`Classes available -> \`${pClasses.map(e => e.name).join('|')}\`\nAdd the class name to get more information about this class. If you want to select this class, add \`--select\` after the name of the class.`);
-    if (!className.toLowerCase().match(/^(clerc)$/)) return message.util!.send(`This class doesn\'t exist (classes available -> \`${pClasses.map(e => e.name).join('|')}\`).`);
-
-    const classInfo = pClasses.find(e => e.name.toLowerCase() == className.toLowerCase());
-    const classImg: any = new MessageAttachment(`./src/game/data/img/${className.toLowerCase()}.png`);
-
-    if (select) {
-      const player = await this.client.player.get(message.member!);
-      if (player) return message.util!.reply('you can\'t select multiple classes!');
-      await Players.create({
-        id: message.author.id,
-        guildId: message.guild!.id,
-        className: classInfo!.name,
-        attributs: classInfo!.attributs,
-        hp: classInfo!.hp,
-        gear: classInfo!.gear
-      });
-      return message.util!.send(`Congratulation, you are now a \`${classInfo!.name}\`, you can start your adventure!`);
-    }
-
-    let embed = new MessageEmbed()
-      .attachFiles(classImg)
-      .setColor(classInfo!.color)
-      .setAuthor(classInfo!.name, `attachment://${className.toLowerCase()}.png`)
-      .setDescription(classInfo!.description)
-      .setFooter(`Add --select on your command to select this class`)
-    
-    return message.util!.send(embed);
+  public async exec(message: Message): Promise<Message | Message[]> {
+    const player = await this.client.player.get(message.member!);
+    if (player) return message.util!.reply('you can\'t start multiple adventures');
+    await Players.create({
+      id: message.author.id,
+      guildId: message.guild!.id,
+      hp: 1,
+    });
+    return message.util!.send(`Congratulation, you are ready to start your adventure!`);
   }
 }
--- src/structures/models/Players.ts
+++ src/structures/models/Players.ts
@@ -2,29 +2,14 @@ import { Schema, model, Document } from 'mongoose';
 
 export interface Players extends Document {
   id: string,
-  guildId: string,
-  className: string,
-  quest: {
-    haveQuest: boolean,
-    progress: number,
-    target: string
-  }
-  level: number,
   gold: number,
-  area: string,
   exp: number,
-  totalExp: number,
   hp: number,
-  arpen: number,
   monster: {
     name: string,
     hp: number,
     exp: number,
-    gold: number,
-    area: string,
     att: number,
-    pdr: number,
-    mdr: number,
     description: string,
     level: number
   }
@@ -35,58 +20,19 @@ export interface Players extends Document {
     boots: string,
     weapon: string
   },
-  attributs: {
-    str: number,
-    dex: number,
-    int: number,
-    vit: number,
-    def: number
-  }
 }
 
 const PlayersSchema = new Schema({
   id: String,
-  guildId: String,
-  className: String,
-  quest: {
-    haveQuest: {
-      type: Boolean,
-      default: false
-    },
-    progress: {
-      type: Number,
-      default: 0
-    },
-    target: {
-      type: String,
-      default: "None"
-    }
-  },
   gold: {
     type: Number,
     default: 0
-  },
-  area: {
-    type: String,
-    default: "Starter Area"
   },
   exp: {
     type: Number,
     default: 0
   },
-  totalExp: {
-    type: Number,
-    default: 15
-  },
   hp: Number,
-  arpen: {
-    type: Number,
-    default: 0
-  },
-  level: {
-    type: Number,
-    default: 1
-  },
   monster: {
     name: {
       type: String,
@@ -100,25 +46,9 @@ const PlayersSchema = new Schema({
       type: Number,
       default: 3
     },
-    gold: {
-      type: Number,
-      default: 2
-    },
-    area: {
-      type: String,
-      default: "Starter Area"
-    },
     att: {
       type: Number,
       default: 13
-    },
-    pdr: {
-      type: Number,
-      default: 10
-    },
-    mdr: {
-      type: Number,
-      default: 10
     },
     description: {
       type: String,
@@ -136,13 +66,6 @@ const PlayersSchema = new Schema({
     boots: String,
     weapon: String
   },
-  attributs: {
-    str: Number,
-    dex: Number,
-    int: Number,
-    vit: Number,
-    def: Number
-  }
 });
 
 export const Players = model<Players>('Players', PlayersSchema);
